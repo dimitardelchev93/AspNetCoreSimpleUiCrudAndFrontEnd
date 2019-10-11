26f9775ecb1fefc362be63f4d666349711cc8233
Dimitar Delchev <dimitardelchev93@abv.bg>
2019-10-11 14:47:44 +0300

    Add event controler and basic list page and hardocode some events

---
+++ AspNetCoreSimpleUiCrudAndFrontEnd/Controllers/EventController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreSimpleUiCrudAndFrontEnd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreSimpleUiCrudAndFrontEnd.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+            List<Event> events = new List<Event>
+            {
+                new Event { Topic = "Test topic", Lecturer = new Lecturer { Name = "Test name" }, Location = new Location { City = "Sofia", Street = "Street 1", StreetNumber = "1", PostalCode = "4000" }, StartTime = DateTime.Now.AddDays(5) },
+                new Event { Topic = "Test topic 2", Lecturer = new Lecturer { Name = "Test name 2" }, Location = new Location { City = "Sofia 2", Street = "Street 2", StreetNumber = "2", PostalCode = "4002" }, StartTime = DateTime.Now.AddDays(7) },
+            };
+
+            return View(events);
+        }
+
+        // GET: Event/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Event/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Event/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Event/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Event/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Event/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Event/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
