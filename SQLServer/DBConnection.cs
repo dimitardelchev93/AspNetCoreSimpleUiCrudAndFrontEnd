6d21859db29f617af99bf76653a8cbe9e5acadc2
Dimitar Delchev <dimitardelchev93@abv.bg>
2019-10-25 14:50:18 +0300

    Add SqlServer DBContext

---
+++ SQLServer/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+
+namespace SQLServer
+{
+    public class DBConnection : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Lecturer> Lecturers { get; set; }
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Server=.\SQLExpress;Database=AspNetCoreSimpleUiCrudAndFrontEnd;Trusted_Connection=True;");
+    }
+}
