5895427d6bd19eac1f12aa0b10dc0f4da50feab3
Dimitar Delchev <dimitardelchev93@abv.bg>
2019-12-04 23:42:15 +0200

    Events crud pages and actions

--- AspNetCoreSimpleUiCrudAndFrontEnd/Startup.cs
+++ AspNetCoreSimpleUiCrudAndFrontEnd/Startup.cs
@@ -24,6 +24,7 @@ namespace AspNetCoreSimpleUiCrudAndFrontEnd
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddEntityFrameworkSqlite().AddDbContext<SQLite.DBConnection>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
